889ede96ec2ba57d0200c65d7fa84aa8fc29b799
border-l <138752444+border-l@users.noreply.github.com>
2023-08-30 12:32:30 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -9,4 +9,17 @@ setup(
     install_requires=[
         "colorama"
     ],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ]
 )
