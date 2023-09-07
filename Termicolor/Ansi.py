6f8b70852f3c16220d544168ee7486063de8b473
border-l <borderlexum@gmail.com>
2023-09-07 17:08:47 -0400

    restructure

--- Termicolor/Ansi.py
+++
@@ -1,9 +0,0 @@
-from .Attributes import Attributes
-
-def ansi(*names):
-    combined = ""
-    for name in names:
-        if not name in Attributes:
-            continue
-        combined += Attributes[name]
-    return combined
